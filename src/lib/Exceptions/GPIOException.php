e4f6788fceb0a3e8315e93b145dad543a5134ce2
Razvan <tangent.jotey@gmail.com>
2022-05-13 23:16:17 +0200

    Update GPIOException.php

--- src/lib/Exceptions/GPIOException.php
+++ src/lib/Exceptions/GPIOException.php
@@ -3,12 +3,11 @@
 namespace CatPaw\RaspberryPI\Exceptions;
 
 use Exception;
-use JetBrains\PhpStorm\Pure;
 use Throwable;
 
 class GPIOException extends Exception {
 // Redefine the exception so message isn't optional
-	#[Pure] public function __construct($message, $code = 0, Throwable $previous = null) {
+	public function __construct($message, $code = 0, Throwable $previous = null) {
 		// some code
 
 		// make sure everything is assigned properly
